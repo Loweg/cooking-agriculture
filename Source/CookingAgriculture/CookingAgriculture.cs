61f1d9a2f1ddf7775698adbd3870110f3868a946
Loweg <loweg@bgdn.cc>
2022-11-21 21:30:29 -0800

    Add sugar and flour

--- Source/CookingAgriculture/CookingAgriculture.cs
+++ Source/CookingAgriculture/CookingAgriculture.cs
@@ -1,4 +1,5 @@
 ﻿using RimWorld;
+using System.Collections.Generic;
 using UnityEngine;
 using Verse;
 
@@ -43,8 +44,73 @@ namespace CookingAgriculture {
 		}
 	}
 
-	[DefOf]
+    [StaticConstructorOnStartup]
+    public static class ChunkConstructor {
+        static ChunkConstructor() {
+            List<ThingDef> thingDefs = DefDatabase<ThingDef>.AllDefsListForReading;
+            for (int i = 0; i < thingDefs.Count; ++i) {
+                ThingDef stoneChunk = thingDefs[i];
+                if (stoneChunk.IsWithinCategory(ThingCategoryDefOf.StoneChunks) && !stoneChunk.butcherProducts.NullOrEmpty() && stoneChunk.butcherProducts.FirstOrDefault(t => t.thingDef.IsStuff)?.thingDef is ThingDef firstStuffProduct) {
+                    ConstructChunkProperties(stoneChunk, firstStuffProduct.stuffProps);
+                }
+            }
+            ResourceCounter.ResetDefs();
+        }
+
+        private static void ConstructChunkProperties(ThingDef stoneChunk, StuffProperties referenceProps) {
+            stoneChunk.resourceReadoutPriority = ResourceCountPriority.Middle;
+            stoneChunk.smeltable = false;
+            stoneChunk.stuffProps = new StuffProperties() {
+                stuffAdjective = referenceProps.stuffAdjective,
+                commonality = referenceProps.commonality,
+                categories = new List<StuffCategoryDef> { CA_DefOf.CA_ChunkStone },
+                statOffsets = new List<StatModifier>(),
+                statFactors = new List<StatModifier>(),
+                color = referenceProps.color,
+                constructEffect = referenceProps.constructEffect,
+                appearance = referenceProps.appearance,
+                soundImpactStuff = referenceProps.soundImpactStuff,
+                soundMeleeHitSharp = referenceProps.soundMeleeHitSharp,
+                soundMeleeHitBlunt = referenceProps.soundMeleeHitBlunt
+            };
+            StuffProperties chunkProps = stoneChunk.stuffProps;
+            if (referenceProps.statOffsets != null) {
+                for (int i = 0; i < referenceProps.statOffsets.Count; i++) {
+                    StatModifier statOffset = referenceProps.statOffsets[i];
+                    chunkProps.statOffsets.Add(new StatModifier() {
+                        stat = statOffset.stat,
+                        value = statOffset.value
+                    });
+                }
+            }
+            if (referenceProps.statFactors != null) {
+                for (int i = 0; i < referenceProps.statFactors.Count; i++) {
+                    StatModifier statFactor = referenceProps.statFactors[i];
+                    chunkProps.statFactors.Add(new StatModifier() {
+                        stat = statFactor.stat,
+                        value = statFactor.value
+                    });
+                }
+            }
+            ModifyStatModifier(ref chunkProps.statFactors, StatDefOf.WorkToMake, ToStringNumberSense.Factor, 1.5f);
+            ModifyStatModifier(ref chunkProps.statFactors, StatDefOf.WorkToBuild, ToStringNumberSense.Factor, 1.5f);
+        }
+
+        private static void ModifyStatModifier(ref List<StatModifier> modifierList, StatDef stat, ToStringNumberSense mode, float factor) {
+            StatModifier statModifier = modifierList.FirstOrDefault((s => s.stat == stat));
+            if (statModifier != null)
+                statModifier.value *= factor;
+            else
+                modifierList.Add(new StatModifier() {
+                    stat = stat,
+                    value = (mode == ToStringNumberSense.Factor ? 1f : 0.0f) * factor
+                });
+        }
+    }
+
+    [DefOf]
 	public static class CA_DefOf {
 		public static JobDef CA_TakeFromSaltPan;
-	}
+        public static StuffCategoryDef CA_ChunkStone;
+    }
 }
